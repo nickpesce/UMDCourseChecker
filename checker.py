1de0e1d2d5a8ac73392a25418a426b6fe8b1593f
Nick Pesce <npesce@terpmail.umd.edu>
2016-04-14 23:54:21 +0000

    Support sending to multiple emails and handle connection problems

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 import urllib2
 import smtplib
+import sys
 import time
 from bs4 import BeautifulSoup
 from email.mime.text import MIMEText
@@ -9,16 +10,16 @@ if len(sys.argv) < 3:
     print "USAGE: " + sys.argv[0] + " course_code sections..." 
     sys.exit(2)
 
-#Address to send email to
-receiver = <RECIEVER EMAIL>
+#Address's to send email to
+receivers = !OMMITTED!
 #Address that the email is sent from
-sender = <SENDER EMAIL>
+sender = !OMMITTED!
 #Password of the sender email
-password = <SENDER PASSWORD>
+password = !OMMITTED!
 #Course code to check for
 course = sys.argv[1]
 #Section numbers to check
-sections = argv[2:]
+sections = sys.argv[2:]
 #List of sections that were already emailed about
 already_emailed = []
 #Current term code
@@ -31,8 +32,12 @@ url = "https://ntst.umd.edu/soc/search?courseId="+course+"&sectionId=&termId="+t
 
 #Continuously check untill program is stopped by user
 while(True):
-    print "Checking status of " + course + " sections " + " ,".join(sections)
-    soup = BeautifulSoup(urllib2.urlopen(url).read(), 'html.parser')
+    print "Checking status of " + course + " section(s) " + " ,".join(sections)
+    try:
+        soup = BeautifulSoup(urllib2.urlopen(url).read(), 'html.parser')
+    except:
+        print "Could not connect"
+        pass
     #All of the tags that contain section ids on the page
     id_tags = soup.find_all("span", class_="section-id")
     #All of the tags that contain open seat counts
@@ -40,26 +45,28 @@ while(True):
     for i in range(0, len(open_tags)):
         open_tag = open_tags[i]
         open = unicode(open_tag.string)
-        if int(open) > 0:
+        if int(open) == 0:
             #Corresponding section number
             section = id_tags[i].string.strip()
             if section in sections and section not in already_emailed:
                 #Make message
-                msg = MIMEText("<strong>"+course + " section " + section + " has " + open + " spots open!</strong>\nLink to add/drop:  https://ntst.umd.edu/testudo/main/dropAdd?venusTermId="+term+"&crslist=" + course + "/"+section)
-                #set header
-                msg['Subject'] = "QUICKLY! There is an opening in " + course + "!"
-                msg['From'] = sender 
-                msg['To'] = receiver
+                body = "Link to add/drop:  https://ntst.umd.edu/testudo/main/dropAdd?venusTermId="+term+"&crslist=" + course + "/"+section + " . " + course + " section " + section + " has " + open + " spots open!\n"
+                for receiver in receivers:
+                    msg = MIMEText(body)
+                    #set header
+                    msg['Subject'] = "QUICKLY! There is an opening in " + course + "!"
+                    msg['From'] = sender 
+                    msg['To'] = receiver
 
-                #Send the email
-                s = smtplib.SMTP("smtp.gmail.com", 587)
-                s.ehlo()
-                s.starttls()
-                s.login(sender, password)
-                s.sendmail(sender, receiver, msg.as_string())
-                s.quit()
+                    #Send the email
+                    s = smtplib.SMTP("smtp.gmail.com", 587)
+                    s.ehlo()
+                    s.starttls()
+                    s.login(sender, password)
+                    s.sendmail(sender, receiver, msg.as_string())
+                    s.quit()
                 already_emailed.append(section);
-                print "Emailed " + receiver + " becuase section " + section + " of " + course + " now has " + open + " spots."
+                print "Emailed " + " & ".join(receivers) + " becuase section " + section + " of " + course + " now has " + open + " spots."
             else:
                 #If the section is full, it should be able to be emailed about again(If it was already emailed about)
                 try:
