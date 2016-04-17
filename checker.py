741bce47c9527b59e420fbf8c23003608e7d8844
Nick Pesce <npesce@terpmail.umd.edu>
2016-04-17 05:58:54 +0000

    Added command line arguments for which email(s) to send to. Also when no section is given, all are matched now.

--- checker.py
+++ checker.py
@@ -1,31 +1,49 @@
 import urllib2
 import smtplib
 import sys
+import getopt
 import time
 from bs4 import BeautifulSoup
 from email.mime.text import MIMEText
 
-#check that correct num commands are given 
-if len(sys.argv) < 3: 
-    print "USAGE: " + sys.argv[0] + " course_code sections..." 
-    sys.exit(2)
 
 #Address's to send email to
-receivers = !OMMITTED!
+receivers = []
 #Address that the email is sent from
 sender = !OMMITTED!
 #Password of the sender email
 password = !OMMITTED!
 #Course code to check for
-course = sys.argv[1]
+course = None
 #Section numbers to check
-sections = sys.argv[2:]
+sections = None
 #List of sections that were already emailed about
 already_emailed = []
 #Current term code
 term = "201608"
 #Seconds to wait for
 sleep_time = 60
+
+#process args
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "e:")
+except:
+    print "USAGE: " + sys.argv[0] + " (-e email)... course_code [sections...]" 
+    sys.exit(2)
+
+#Must at least give course name
+if(len(args) < 1):
+    print "USAGE: " + sys.argv[0] + " (-e email)... course_code [sections...]" 
+    sys.exit(2)
+
+#Go through each argument
+for o, a in opts:
+    if o == "-e":
+        receivers.append(a)
+
+course = args[0]
+if(len(args) > 1):
+    sections = args[1:]
 
 #Url of course schedule
 url = "https://ntst.umd.edu/soc/search?courseId="+course+"&sectionId=&termId="+term+"&courseStartCompare=&courseStartMin=&courseStartAM="
@@ -45,10 +63,10 @@ while(True):
     for i in range(0, len(open_tags)):
         open_tag = open_tags[i]
         open = unicode(open_tag.string)
-        if int(open) > 0:
+        if int(open) >  0:
             #Corresponding section number
             section = id_tags[i].string.strip()
-            if section in sections and section not in already_emailed:
+            if (sections is None) or (section in sections) and (section not in already_emailed):
                 #Make message
                 body = "Link to add/drop:  https://ntst.umd.edu/testudo/main/dropAdd?venusTermId="+term+"&crslist=" + course + "/"+section + " . " + course + " section " + section + " has " + open + " spots open!\n"
                 for receiver in receivers:
